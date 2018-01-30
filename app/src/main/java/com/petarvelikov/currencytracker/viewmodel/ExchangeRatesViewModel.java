c3c2d5aa064196785aac0f0a37f7e6fb84b49ac1
VelikovPetar <petarvelikov95@gmail.com>
2018-01-30 12:03:06 +0100

    Format code.

--- app/src/main/java/com/petarvelikov/currencytracker/viewmodel/ExchangeRatesViewModel.java
+++ app/src/main/java/com/petarvelikov/currencytracker/viewmodel/ExchangeRatesViewModel.java
@@ -10,8 +10,6 @@ import com.petarvelikov.currencytracker.resources.ExchangeRatesResourcesHelper;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 import javax.inject.Inject;
 
