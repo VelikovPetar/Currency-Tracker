6c43fbfdf94f0bca742bfcc16a692944c7baedf1
VelikovPetar <petarvelikov95@gmail.com>
2018-05-21 20:04:39 +0200

    Update colors

--- app/src/main/java/com/petarvelikov/currencytracker/view/activity/MainActivity.java
+++ app/src/main/java/com/petarvelikov/currencytracker/view/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.petarvelikov.currencytracker.R;
@@ -23,18 +24,22 @@ public class MainActivity extends AppCompatActivity {
   }
 
   private void bindUi(Bundle savedInstanceState) {
+    setupToolbar();
     BottomNavigationView navigationView = findViewById(R.id.bottomNavigationView);
     navigationView.setOnNavigationItemSelectedListener(item -> {
       switch (item.getItemId()) {
         case R.id.actionCryptoCurrencies:
           replaceFragment(new CryptoCurrenciesFragment());
+          setToolbarTitle(R.string.crypto_currencies);
           Toast.makeText(MainActivity.this, R.string.crypto_currencies, Toast.LENGTH_SHORT).show();
           break;
         case R.id.actionExchangeRates:
           replaceFragment(new ExchangeRatesFragment());
+          setToolbarTitle(R.string.exchange_rates);
           Toast.makeText(MainActivity.this, R.string.exchange_rates, Toast.LENGTH_SHORT).show();
           break;
         case R.id.actionTransactions:
+          setToolbarTitle(R.string.transactions);
           replaceFragment(new TransactionsFragment());
           break;
       }
@@ -45,6 +50,18 @@ public class MainActivity extends AppCompatActivity {
     if (savedInstanceState == null) {
       navigationView.setSelectedItemId(R.id.actionCryptoCurrencies);
       replaceFragment(new CryptoCurrenciesFragment());
+    }
+  }
+
+  private void setupToolbar() {
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    setToolbarTitle(R.string.crypto_currencies);
+  }
+
+  private void setToolbarTitle(int stringId) {
+    if (getSupportActionBar() != null) {
+      getSupportActionBar().setTitle(stringId);
     }
   }
 
